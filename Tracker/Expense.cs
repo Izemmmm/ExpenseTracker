a7925fd893994d8a3422d867de63fd7e1d6f0bcb
Volkov <ilya.volkov@epg.com>
2026-01-06 23:17:32 +0100

    Expnse class created

---
+++ Tracker/Expense.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpenseTracker;
+
+public class Expense
+{
+    public int Amount {get; set;}
+    public string Description {get; set;}
+    public DateTime Date {get; set;} = DateTime.Now;
+
+    public Expense(int amount, string description = "")
+    {
+        Amount = amount;
+        Description = description;
+    }
+
+    public Expense(int amount, DateTime date, string description = "") : this(amount, description)
+    {
+        Date = date;
+    }
+}
