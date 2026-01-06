1a49a41edbbbfd5f55815b8e7badc2d0e47df843
Volkov <ilya.volkov@epg.com>
2026-01-06 23:40:20 +0100

    ExpenseList class added

---
+++ Tracker/ExpenseList.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Tracker;
+
+public class ExpenseList
+{
+    List<Expense> Expenses {get; } = new List<Expense>();
+
+    public void AddExpense(int amount, string description = "")
+    {
+        Expense newExpense = new Expense(amount, description);
+        Expenses.Add(newExpense);
+    }
+}
