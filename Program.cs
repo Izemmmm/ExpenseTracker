ebb32610e73a12b0220f64aab4f34ddb8bacfad7
Volkov <ilya.volkov@epg.com>
2026-01-06 22:50:37 +0100

    Welcome  line added

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using ExpenseTracker.Enums;
 
 MenuOption chosenOption = MenuOption.Default;
-string input = "";
+string? input = "";
+
+Console.WriteLine("Welcome to Expense tracker!");
+Console.WriteLine("Menu:");
+Console.WriteLine("1. Add new expense");
+Console.WriteLine("2. View expenses");
+Console.WriteLine("3. Exit");
 
 while(chosenOption != MenuOption.Exit)
 {
+    input = Console.ReadLine();
     if(!Enum.TryParse(input, out chosenOption))
     {
         Console.WriteLine("Incorrect option, please try again!");
